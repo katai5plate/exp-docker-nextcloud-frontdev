522b261145827c104e8e8913cf556b852520addb
katai5plate <katai5plate.sub@gmail.com>
2020-07-08 14:33:18 +0900

    bugfix

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # exp-docker-nextcloud-frontdev
-dockerでNextCloudのフロント開発環境を構築してみるテスト
+
+docker で NextCloud のフロント開発環境を構築してみるテスト
+
+とりあえず webpack がビルドに成功するところまで。
 
 ## usage
+
 ```
 docker-compose up -d && docker ps
 docker exec -it XXXXXXXX bash
-apt update && apt -y upgrade && apt install -y git node npm
-cd /var/www/html && bash ./__front_dev/install.sh
-npm i
+cd /var/www/html && bash /__front_dev/install.sh
+```
+
+```
+npm run watch
 ```
--- data/install.sh
+++ data/install.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/nextcloud/server
+apt update
+apt -y upgrade
+apt install -y git nodejs npm
+npm i npm@latest -g
 
-cp ./server/.editorconfig ./.editorconfig
-cp ./server/.jshintrc ./.jshintrc
-cp ./server/package.json ./package.json
-cp ./server/package-lock.json ./package-lock.json
-cp ./server/webpack.common.js ./webpack.common.js
-cp ./server/webpack.dev.js ./webpack.dev.js
-cp ./server/webpack.prod.js ./webpack.prod.js
-cp ./server/babel.config.js ./babel.config.js
+rm -rv ./__front_dev_temp
 
-cp ./server/core/webpack.js ./core/webpack.js
-cp ./server/apps/accessibility/webpack.js ./apps/accessibility/webpack.js
-cp ./server/apps/comments/webpack.js ./apps/comments/webpack.js
-cp ./server/apps/files/webpack.js ./apps/files/webpack.js
-cp ./server/apps/files_sharing/webpack.js ./apps/files_sharing/webpack.js
-cp ./server/apps/files_trashbin/webpack.js ./apps/files_trashbin/webpack.js
-cp ./server/apps/files_versions/webpack.js ./apps/files_versions/webpack.js
-cp ./server/apps/oauth2/webpack.js ./apps/oauth2/webpack.js
-cp ./server/apps/settings/webpack.js ./apps/settings/webpack.js
-cp ./server/apps/systemtags/webpack.js ./apps/systemtags/webpack.js
-cp ./server/apps/twofactor_backupcodes/webpack.js ./apps/twofactor_backupcodes/webpack.js
-cp ./server/apps/updatenotification/webpack.js ./apps/updatenotification/webpack.js
-cp ./server/apps/workflowengine/webpack.js ./apps/workflowengine/webpack.js
+mkdir __front_dev_temp
+git clone --depth 1 https://github.com/nextcloud/server ./__front_dev_temp/server
+git clone --depth 1 https://github.com/nextcloud/3rdparty ./__front_dev_temp/3rdparty
 
-cp -r ./server/apps/accessibility/src ./apps/accessibility/src
-cp -r ./server/apps/comments/src ./apps/comments/src
-cp -r ./server/core/src ./core/src
-cp -r ./server/apps/files/src ./apps/files/src
-cp -r ./server/apps/files_sharing/src ./apps/files_sharing/src
-cp -r ./server/apps/files_trashbin/src ./apps/files_trashbin/src
-cp -r ./server/apps/files_versions/src ./apps/files_versions/src
-cp -r ./server/apps/oauth2/src ./apps/oauth2/src
-cp -r ./server/apps/settings/src ./apps/settings/src
-cp -r ./server/apps/systemtags/src ./apps/systemtags/src
-cp -r ./server/apps/twofactor_backupcodes/src ./apps/twofactor_backupcodes/src
-cp -r ./server/apps/updatenotification/src ./apps/updatenotification/src
-cp -r ./server/apps/workflowengine/src ./apps/workflowengine/src
+cp ./__front_dev_temp/server/.editorconfig ./.editorconfig
+cp ./__front_dev_temp/server/.jshintrc ./.jshintrc
+cp ./__front_dev_temp/server/package.json ./package.json
+cp ./__front_dev_temp/server/package-lock.json ./package-lock.json
+cp ./__front_dev_temp/server/webpack.common.js ./webpack.common.js
+cp ./__front_dev_temp/server/webpack.dev.js ./webpack.dev.js
+cp ./__front_dev_temp/server/webpack.prod.js ./webpack.prod.js
+cp ./__front_dev_temp/server/babel.config.js ./babel.config.js
 
-rm -rv ./server
+cp ./__front_dev_temp/server/core/webpack.js ./core/webpack.js
+cp ./__front_dev_temp/server/apps/accessibility/webpack.js ./apps/accessibility/webpack.js
+cp ./__front_dev_temp/server/apps/comments/webpack.js ./apps/comments/webpack.js
+cp ./__front_dev_temp/server/apps/files/webpack.js ./apps/files/webpack.js
+cp ./__front_dev_temp/server/apps/files_sharing/webpack.js ./apps/files_sharing/webpack.js
+cp ./__front_dev_temp/server/apps/files_trashbin/webpack.js ./apps/files_trashbin/webpack.js
+cp ./__front_dev_temp/server/apps/files_versions/webpack.js ./apps/files_versions/webpack.js
+cp ./__front_dev_temp/server/apps/oauth2/webpack.js ./apps/oauth2/webpack.js
+cp ./__front_dev_temp/server/apps/settings/webpack.js ./apps/settings/webpack.js
+cp ./__front_dev_temp/server/apps/systemtags/webpack.js ./apps/systemtags/webpack.js
+cp ./__front_dev_temp/server/apps/twofactor_backupcodes/webpack.js ./apps/twofactor_backupcodes/webpack.js
+cp ./__front_dev_temp/server/apps/updatenotification/webpack.js ./apps/updatenotification/webpack.js
+cp ./__front_dev_temp/server/apps/workflowengine/webpack.js ./apps/workflowengine/webpack.js
+
+cp -r ./__front_dev_temp/server/apps/accessibility/src ./apps/accessibility/src
+cp -r ./__front_dev_temp/server/apps/comments/src ./apps/comments/src
+cp -r ./__front_dev_temp/server/core/src ./core/src
+cp -r ./__front_dev_temp/server/apps/files/src ./apps/files/src
+cp -r ./__front_dev_temp/server/apps/files_sharing/src ./apps/files_sharing/src
+cp -r ./__front_dev_temp/server/apps/files_trashbin/src ./apps/files_trashbin/src
+cp -r ./__front_dev_temp/server/apps/files_versions/src ./apps/files_versions/src
+cp -r ./__front_dev_temp/server/apps/oauth2/src ./apps/oauth2/src
+cp -r ./__front_dev_temp/server/apps/settings/src ./apps/settings/src
+cp -r ./__front_dev_temp/server/apps/systemtags/src ./apps/systemtags/src
+cp -r ./__front_dev_temp/server/apps/twofactor_backupcodes/src ./apps/twofactor_backupcodes/src
+cp -r ./__front_dev_temp/server/apps/updatenotification/src ./apps/updatenotification/src
+cp -r ./__front_dev_temp/server/apps/workflowengine/src ./apps/workflowengine/src
+
+cp -r ./__front_dev_temp/3rdparty/* ./3rdparty/
+
+rm -rv ./__front_dev_temp
+
+npm i
